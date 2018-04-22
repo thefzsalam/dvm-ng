f42c9c10ba4038fdd6a7be528b35656484488af0
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-22 18:10:43 +0530

    Just creates a gtk::Application and loads main_login_view.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 extern crate gtk;
 extern crate gio;
-
 use gtk::*;
 use gio::*;
-
 mod views;
+use views::*;
+mod main_app;
+use main_app::*;
+use std::rc::Rc;
 
-use views::MainLoginView;
-
+const MAIN_APP:MainApp = MainApp {};
 
 
 fn activate(app: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(&app);
-
     let css_provider = CssProvider::new();
     css_provider.load_from_data(include_str!("glade_ui/styles.css").as_bytes());
     StyleContext::add_provider_for_screen(&window.get_screen().unwrap(),&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    let main_root = MainLoginView::new(|userid,password| {
-
-    });
-
-    window.add(&main_root);
+    let main_root = MainLoginView::new(Rc::new(MAIN_APP));
+    window.add(main_root.get_root_container());
     app.add_window(&window);
     window.present();
 }
