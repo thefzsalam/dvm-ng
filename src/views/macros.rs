18bbcbab98d251a36f71c98885217c09be596232
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-05-04 19:01:14 +0530

    Added comments to explain the usage of the ui_struct! macro

--- src/views/macros.rs
+++ src/views/macros.rs
@@ -1,5 +1,12 @@
 #[macro_export]
 
+/**
+ * Macro to add a constructor method named `build`
+ * which takes a gtk::Builder and initialize the members
+ * using it.
+ * All members must have a corresponding widget in the UI file loaded into Builder,
+ * such that the name of the member is the id of the widget.
+ */
 macro_rules! ui_struct {
     (struct $struct_name:ident {
         $($i:ident: $t:ty),+
